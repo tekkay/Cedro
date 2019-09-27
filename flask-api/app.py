81bc54feeac9c3296b294a1d227bfba36b48425d
tekkay <wellingtontekay@gmail.com>
2019-09-27 11:47:00 -0300

    Add files via upload

--- flask-api/app.py
+++ flask-api/app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify
 import requests
 import bs4
 from flask import Flask
+import re
 
 noticia_ruin = [
     'morte',
@@ -17,12 +18,12 @@ app = Flask(__name__)
 
 nome_delinquente = ('osama') 
                
-@app.route('/nome_delinquente', methods=['GET'])
+@app.route('/', methods=['GET']) #era pra mudar a rota junto com a request.get, porém não consegui que funcionasse.
 def crawler():
     res = requests.get("https://g1.globo.com/busca/?q=" + nome_delinquente)
     res.text
     soup = bs4.BeautifulSoup(res.text, 'lxml')
-    for link in soup.find_all('a', href=True):
+    for link in soup.find_all(href=True and re.compile(noticia_ruin)):
         print(link['href'])
     return jsonify(link['href'])
         
